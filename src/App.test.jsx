bfe67d47826c7c497c89be91f68a44da05639cc3
armaBurton <armanarma@armaburton.com>
2022-04-25 17:13:17 -0700

    tests are passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,28 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
-test('Should render the header', () => {
 
+describe('App', () => {
+  it('should render the header', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+
+    // screen.getByRole('backgroundColor', { backgroundColor: '#eee' });
+
+    screen.getByAltText(/alchemy logo/i);
+
+    // screen.getByText(/vonta/i);
+    return waitFor(() => { 
+      const profile = screen.getByText(/vonta/i);
+      expect(profile.textContent).toBe('Vonta');
+    });
+
+  })
 })
+
+
--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,3 +1,9 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import Home from './Home';
+import App from '../../App';
+
 
 const user = {
   id: 1,
@@ -10,6 +16,48 @@ const user = {
   color: 'crimson',
 }
 
-test('Should render the user profile', () => {
+describe('Home', () => {
+  it('should poop it\'s pants', () => {
+    // test to see if test is running
+    console.log('poop your pants');
+  })
 
+  it('should render the user profile', async () => {
+
+    render(
+      <MemoryRouter>
+        <Home user={user} />
+      </MemoryRouter>
+    )
+
+    screen.getByText(/Vonta/i);
+
+    screen.getByText(/res non verba/i);
+
+    waitFor(() => {
+      const heading = screen.getByRole('heading');
+      expect(heading.textContent).toBe(/interests/i);
+    })
+
+    // const avatar = screen.getByAltText(/avatar/i);
+    const avatar = screen.getByRole('img', {name: /avatar/i});
+    expect(avatar.src).toEqual('https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif');
+
+    const header = screen.getByRole('img', {name: /header/i});
+    expect(header.src).toEqual('https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png');
+
+    const likeArr = [];
+    const userLikes = screen.getAllByRole('listitem');
+    userLikes.map(like => {
+      return likeArr.push(like.textContent)
+    });
+    expect(likeArr).toEqual([
+      'React',
+      'Anime',
+      'Traveling',
+      'Living',
+      'Tower Defense Games',
+      'Card Games'
+    ]);
+  })
 })
